7647e16925b1de5028c93ef3a1808382e3acf5ea
locky5 <daikevin19@yahoo.com>
2019-07-19 11:47:49 -0400

    Done.

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,32 +1,40 @@
 import React from 'react'
 import Purse from "./Purse"
 import InventoryTile from "./InventoryTile"
+import PotionTile from "./PotionTile"
 
 
-const Inventory = (props) => {
+class Inventory extends React.Component {
 
-  const renderInventory = () => {
-
-    return props.inventory.map(item => {
-      const potion = props.potions.find(potion => potion.id === item.id)
-
-       return (
-        <InventoryTile potion={potion} sell={props.sell}>
-          <h4>{potion.name} x {item.amount}</h4>
-        </InventoryTile>
-      )
-    })  
+  state = {
+    className: "inventory-card",
+    action: "sell"
   }
 
-  return (
-    <div className="inventory">
-      <h2>Inventory</h2>
-      <Purse stopShake={props.stopShake} shake={props.shake} gold={props.gold}/>
-      <div className="inventory-cards">
-       {renderInventory()}
+  renderInventory = () => {
+
+    return this.props.inventory.map(item => {
+      const potion = this.props.potions.find(potion => potion.id === item.id)
+
+       return (
+        <PotionTile potion={potion} sell={this.props.sell} oneClassName={this.state.className} action={this.state.action}>
+          <h4>{potion.name} x {item.amount}</h4>
+        </PotionTile>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <Purse stopShake={this.props.stopShake} shake={this.props.shake} gold={this.props.gold}/>
+        <div className="inventory-cards">
+         {this.renderInventory()}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Inventory
--- src/Shop.js
+++ src/Shop.js
@@ -1,31 +1,37 @@
 import React from 'react'
-import ShopTile from "./ShopTile"
+import PotionTile from "./PotionTile"
 
-const Shop = (props) => {
+class Shop extends React.Component {
 
-  const renderPotions = () => {
-    return props.potions.map(potion => {
-      return ( 
-        <ShopTile potion={potion} addToInventory={props.addToInventory}>
+  state = {
+    className: "card",
+    action: "addToInventory"
+  }
+
+  renderPotions = () => {
+    return this.props.potions.map(potion => {
+      return (
+        <PotionTile potion={potion} addToInventory={this.props.addToInventory} oneClassName={this.state.className} action={this.state.action}>
           <h4 className="potion-header">{potion.name}</h4>
-        </ShopTile>
+        </PotionTile>
       )
     })
   }
 
-  return (
-    <div className="shop">
-      <div className="shop-header">
-        <img className="torch" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
-        <h1>Ye Olde Apothecary</h1>
-        <img className="torch flipped" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
+  render() {
+    return (
+      <div className="shop">
+        <div className="shop-header">
+          <img className="torch" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
+          <h1>Ye Olde Apothecary</h1>
+          <img className="torch flipped" src="https://img.itch.zone/aW1nLzEzNDg2ODkuZ2lm/original/BEjCgg.gif" alt="torch"/>
+        </div>
+        <div className="shop-cards">
+          {this.renderPotions()}
+        </div>
       </div>
-      <div className="shop-cards">
-        {renderPotions()}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Shop
-
